21b5ca32cc6cb19bf298b9b448c73cc082d858e7
ntabucejo <ntabucejo@gmail.com>
2022-11-24 20:41:13 +0800

    feature(franchise): implement image and video hover

--- components/sections/franchise/card.tsx
+++ components/sections/franchise/card.tsx
@@ -1,16 +1,30 @@
 import Image from "next/image";
 
-const Card = () => {
+type Props = {
+  name: string;
+  image: string;
+  video: string;
+};
+
+const Card = ({ image, video, name }: Props) => {
   return (
-    <li className="grid aspect-video items-center rounded bg-zinc-900">
-      <div className="relative mx-auto h-1/2 w-1/2">
+    <li className="group relative z-20 grid aspect-video cursor-pointer items-center overflow-hidden rounded">
+      <div className="transition-smooth absolute inset-0 bg-zinc-900 hover:bg-transparent" />
+      <div className="pointer-events-none relative mx-auto h-1/2 w-1/2">
         <Image
-          src="/assets/images/disney-plus-logo.svg"
+          src={`/assets/images/${image}.png`}
           fill
-          alt="Franchise"
+          alt={name}
           className="object-contain"
         />
       </div>
+      <video
+        className="absolute -z-10 h-full brightness-50"
+        autoPlay
+        loop
+        muted>
+        <source src={`/assets/videos/${video}.mp4`} type="video/mp4" />
+      </video>
     </li>
   );
 };
--- components/sections/franchise/index.tsx
+++ components/sections/franchise/index.tsx
@@ -2,15 +2,50 @@ import React from "react";
 import Card from "./card";
 
 const Franchise = () => {
+  const list = [
+    {
+      id: 1,
+      name: "Disney",
+      image: "disney-logo",
+      video: "disney-opening",
+    },
+    {
+      id: 2,
+      name: "Pixar",
+      image: "pixar-logo",
+      video: "pixar-opening",
+    },
+    {
+      id: 3,
+      name: "Marvel",
+      image: "marvel-logo",
+      video: "marvel-opening",
+    },
+    {
+      id: 4,
+      name: "Starwars",
+      image: "starwars-logo",
+      video: "starwars-opening",
+    },
+    {
+      id: 5,
+      name: "National Geographic",
+      image: "national-geographic-logo",
+      video: "national-geographic-opening",
+    },
+    {
+      id: 6,
+      name: "Star",
+      image: "star-logo",
+      video: "star-opening",
+    },
+  ];
   return (
     <section className="pr-4">
-      <ul className="grid grid-cols-[repeat(6,1fr)] gap-2 overflow-x-auto">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <ul className="grid grid-cols-[repeat(6,1fr)] gap-2 overflow-auto">
+        {list.map(({ id, name, image, video }) => (
+          <Card key={id} name={name} image={image} video={video} />
+        ))}
       </ul>
     </section>
   );

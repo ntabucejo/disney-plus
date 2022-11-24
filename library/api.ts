5ebd4520cb1f77569bcbe543bc54541c5ea1cd8a
ntabucejo <ntabucejo@gmail.com>
2022-11-25 01:39:20 +0800

    chore(api): add get movies

--- library/api.ts
+++ library/api.ts
@@ -3,8 +3,40 @@ import type { Logo, Movie } from "../types";
 const TMDB_API_KEY = process.env.TMDB_API_KEY!;
 const TMDB_API_URL = process.env.TMDB_API_URL!;
 
+type Group = "popular" | "top-rated";
+
 const api = {
   get: {
+    movies: async ({
+      group,
+      page,
+    }: {
+      group: Group;
+      page: number;
+    }): Promise<Movie[]> => {
+      const groupName = group.split("-").join("_");
+      const response = await fetch(
+        `${TMDB_API_URL}/3/movie/${groupName}?api_key=${TMDB_API_KEY}&language=en-US&page=${page}`
+      );
+      const { results } = await response.json();
+      const movies = results.map((movie: any) => {
+        return {
+          id: movie.id,
+          title: movie.title,
+          isForAdult: movie.adult,
+          image: {
+            poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            backdrop: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
+          },
+          overview: movie.overview,
+          releasedAt: movie.release_date,
+          language: {
+            original: movie.original_language,
+          },
+        };
+      });
+      return movies;
+    },
     movie: {
       logo: async (movieId: string): Promise<Logo> => {
         const response = await fetch(

3984f9b2826a602ea0b6fd6d36c2529dbe3b1e7f
ntabucejo <ntabucejo@gmail.com>
2022-11-24 15:16:27 +0800

    chore(home): add page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import Billboard from "../components/sections/billboard";
+import Overview from "../components/sections/overview";
+import Franchise from "../components/sections/franchise";
+import Content from "../components/wrappers/content";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <>
+      <Billboard />
+      <Content variant="primary">
+        <Overview />
+        <Franchise />
+      </Content>
+    </>
+  );
 };
 
 export default Page;

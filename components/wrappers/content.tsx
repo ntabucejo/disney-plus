53e1eceb6d773c14839295992cb35cde33160079
ntabucejo <ntabucejo@gmail.com>
2022-11-24 15:16:55 +0800

    chore(content): add dynamic props

--- components/wrappers/content.tsx
+++ components/wrappers/content.tsx
@@ -2,11 +2,24 @@ import type { ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
+  variant: "primary" | "secondary" | "tertiary";
 };
 
-const Content = ({ children }: Props) => {
+const Content = ({ children, variant }: Props) => {
+  let style = "";
+
+  switch (variant) {
+    case "primary":
+      style = "tablet:-mt-[250px] laptop:-mt-[450px] desktop:-mt-[600px]";
+      break;
+    case "secondary":
+      break;
+    case "tertiary":
+      break;
+  }
+
   return (
-    <div className="transition-smooth tablet:-mt-[250px] laptop:-mt-[450px] desktop:-mt-[600px]">
+    <div className={`${style} transition-smooth space-y-12 pb-12 pr-4`}>
       {children}
     </div>
   );

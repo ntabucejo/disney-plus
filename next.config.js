cb53b06932c5b6c3c88472cbafd7367a8f2dd298
ntabucejo <ntabucejo@gmail.com>
2022-11-24 11:15:50 +0800

    chore(config): add trusted domains

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true },
+  images: {
+    domains: ["image.tmdb.org", "img1.hotstarext.com"],
+  },
 };
 
 module.exports = nextConfig;

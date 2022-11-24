5d2dc35dc3325178dcc33f62c8a5695e78cb8503
ntabucejo <ntabucejo@gmail.com>
2022-11-24 11:13:34 +0800

    chore(prettier): add prettier and setup plugin for tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxBracketSameLine: true,
+  bracketSameLine: true,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};

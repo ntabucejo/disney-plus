1fd7e4e7dfbce38cb4667f5ddf3d01a2ff14e421
ntabucejo <ntabucejo@gmail.com>
2022-11-25 08:06:56 +0800

    fix(billboard): remove left fade

--- components/sections/billboard/index.tsx
+++ components/sections/billboard/index.tsx
@@ -8,7 +8,7 @@ const Billboard = ({ image }: Props) => {
   return (
     <section className="sticky top-0 -z-10 aspect-video max-h-screen w-full">
       <Image src={image} fill alt="Showcase" className="object-cover" />
-      <div className="absolute inset-0 z-10 bg-gradient-to-r from-background-dark to-transparent" />
+      <div className="absolute inset-0 z-10 hidden bg-gradient-to-r from-background-dark to-transparent tablet:block" />
       <div className="absolute inset-0 z-10 bg-gradient-to-t from-background-dark to-transparent" />
     </section>
   );

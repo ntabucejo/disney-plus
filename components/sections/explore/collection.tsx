1bf7f4f651b85dbc950dff46c3786df53d396be4
ntabucejo <ntabucejo@gmail.com>
2022-11-25 08:24:21 +0800

    chore(font): modify font size to base

--- components/sections/explore/collection.tsx
+++ components/sections/explore/collection.tsx
@@ -9,7 +9,7 @@ type Props = {
 const Collection = ({ title, movies }: Props) => {
   return (
     <div className="space-y-4">
-      <strong className="text-lg tablet:text-2xl">{title}</strong>
+      <strong className="tablet:text-2xl">{title}</strong>
       <ul className="grid grid-flow-col gap-2 tablet:grid-cols-[repeat(4,1fr)] laptop:grid-cols-[repeat(6,1fr)] desktop:grid-cols-[repeat(8,1fr)]">
         {movies.map((movie, index) => (
           <div

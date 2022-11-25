72dbd4efb3f4bc9e1fed3415117c8de6ac929346
ntabucejo <ntabucejo@gmail.com>
2022-11-26 03:10:13 +0800

    fix(ui): fix general ui

--- components/sections/showcase/index.tsx
+++ components/sections/showcase/index.tsx
@@ -15,7 +15,8 @@ const Showcase = async ({ media }: Props) => {
   const language = convertLanguage(media.language!.original!);
 
   return (
-    <section className="space-y-4 tablet:max-w-md">
+    <section className="tablet:max-w-md">
+      {/* Logo */}
       <div className="relative aspect-video w-full">
         <Image
           src={logo!.image!}
@@ -25,21 +26,28 @@ const Showcase = async ({ media }: Props) => {
           className="object-contain"
         />
       </div>
-      <div className="flex items-center gap-1 text-xs tablet:gap-2 tablet:text-base">
-        <p className="font-semibold">
-          {media.releasedAt?.slice(0, 4)} • {humanizeRuntime(measure)} •{" "}
-          {language?.en.name} •
-        </p>
-        <div className="rounded bg-rated-dark px-2 py-0.5 font-semibold tablet:py-0">
-          {media.isForAdult ? "18+" : "PG"}
+      <div className="space-y-6">
+        <div className="space-y-4">
+          {/* Details */}
+          <div className="flex items-center gap-1 text-xs tablet:gap-2 tablet:text-base">
+            <p className="font-semibold">
+              {media.releasedAt?.slice(0, 4)} • {humanizeRuntime(measure)} •{" "}
+              {language?.en.name ? language?.en.name : "English"} •
+            </p>
+            <div className="rounded bg-rated-dark px-2 py-0.5 font-semibold tablet:py-0">
+              {media.isForAdult ? "18+" : "PG"}
+            </div>
+          </div>
+          {/* Overview */}
+          <p className="text-sm tablet:hidden desktop:block desktop:text-base">
+            {media.overview!.split(".")[0]}
+          </p>
         </div>
-      </div>
-      <p className="text-sm tablet:hidden desktop:block desktop:text-base">
-        {media.overview!}
-      </p>
-      <div className="flex gap-4">
-        <Button variant={{ name: "primary", type: "play" }} />
-        <Button variant={{ name: "primary", type: "save" }} />
+        {/* Actions */}
+        <div className="flex gap-4">
+          <Button variant={{ name: "primary", type: "play" }} />
+          <Button variant={{ name: "primary", type: "save" }} />
+        </div>
       </div>
     </section>
   );

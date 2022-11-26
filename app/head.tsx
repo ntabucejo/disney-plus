d5a7b3b275b2241b801eaa4f354068f6cbe16e40
ntabucejo <ntabucejo@gmail.com>
2022-11-27 04:18:05 +0800

    chore(seo): add meta description

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,10 @@ const Head = () => {
       <title>Disney+</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/disney-plus-icon.svg" />
+      <meta
+        name="description"
+        content="Disney+ clone with Nextjs and Talwindcss by Nikko Abucejo"
+      />
     </>
   );
 };

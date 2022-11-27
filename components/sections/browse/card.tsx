fc35df0827151ef868e90cfadcf78a6527ca0f6f
ntabucejo <ntabucejo@gmail.com>
2022-11-27 18:42:32 +0800

    fix(card): fix mobile responsive

--- components/sections/browse/card.tsx
+++ components/sections/browse/card.tsx
@@ -14,33 +14,52 @@ const Card = async ({ media }: Props) => {
   const measure = await api.get.media.measure({ type, id });
 
   return (
-    <li className="space-y-2">
-      <Link href={`/${media.type}/${media.id}`}>
-        <div className="relative aspect-video overflow-hidden rounded">
-          <Image
-            src={`https://image.tmdb.org/t/p/w500${media.image.backdrop!}`}
-            alt={media.title!}
-            fill
-            className="transition-smooth object-cover hover:brightness-[130%]"
-          />
-          <div className="pointer-events-none absolute inset-0 z-10 bg-gradient-to-t from-background-dark to-transparent" />
-        </div>
-        <div className="relative z-10 -mt-6 px-4 pb-2">
-          <p className="font-semibold">{media.title}</p>
-          <small>
-            <p className="font-semibold">
-              {media.releasedAt?.slice(0, 4)
-                ? media.releasedAt?.slice(0, 4)
-                : "New"}{" "}
-              •{" "}
-              {type === "movies"
-                ? humanizeRuntime(measure)
-                : `${measure} Seasons`}
-            </p>
-          </small>
-        </div>
-      </Link>
-    </li>
+    <>
+      {/* Show on tablet */}
+      <li className="hidden space-y-2 tablet:block">
+        <Link href={`/${media.type}/${media.id}`}>
+          <div className="relative aspect-video overflow-hidden rounded">
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${media.image.backdrop!}`}
+              alt={media.title!}
+              fill
+              className="transition-smooth object-cover hover:brightness-[130%]"
+            />
+            <div className="pointer-events-none absolute inset-0 z-10 bg-gradient-to-t from-background-dark to-transparent" />
+          </div>
+          <div className="relative z-10 -mt-6 px-4 pb-2">
+            <p className="font-semibold">{media.title}</p>
+            <small>
+              <p className="font-semibold">
+                {media.releasedAt?.slice(0, 4)
+                  ? media.releasedAt?.slice(0, 4)
+                  : "New"}{" "}
+                •{" "}
+                {type === "movies"
+                  ? humanizeRuntime(measure)
+                  : `${measure} Seasons`}
+              </p>
+            </small>
+          </div>
+        </Link>
+      </li>
+      {/* Show on mobile */}
+      <li className="tablet:hidden">
+        <Link
+          href={`/${media.type}/${media.id}`}
+          className="flex items-center gap-4">
+          <div className="relative aspect-video w-24 flex-none overflow-hidden rounded">
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${media.image.backdrop!}`}
+              alt={media.title!}
+              fill
+              className="transition-smooth object-cover hover:brightness-[130%]"
+            />
+          </div>
+          <p className="text-xs font-semibold">{media.title}</p>
+        </Link>
+      </li>
+    </>
   );
 };
 
--- components/sections/browse/index.tsx
+++ components/sections/browse/index.tsx
@@ -7,9 +7,12 @@ type Props = {
 const Browse = ({ children }: Props) => {
   return (
     <section className="space-y-3 tablet:space-y-4">
-      <ul className="grid gap-x-2 gap-y-8 tablet:grid-cols-[repeat(2,1fr)] tala:grid-cols-[repeat(3,1fr)] laptop:grid-cols-[repeat(4,1fr)] desktop:grid-cols-[repeat(5,1fr)]">
+      {/* Show on tablet */}
+      <ul className="hidden gap-x-2 gap-y-8 tablet:grid tablet:grid-cols-[repeat(2,1fr)] tala:grid-cols-[repeat(3,1fr)] laptop:grid-cols-[repeat(4,1fr)] desktop:grid-cols-[repeat(5,1fr)]">
         {children}
       </ul>
+      {/* Show on mobile */}
+      <ul className="grid gap-4 tablet:hidden">{children}</ul>
     </section>
   );
 };

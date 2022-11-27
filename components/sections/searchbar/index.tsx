a23f7a7ecdcb11d2e95337f795db82edb34c0706
ntabucejo <ntabucejo@gmail.com>
2022-11-27 19:19:35 +0800

    chore(search): add form

--- components/sections/searchbar/index.tsx
+++ components/sections/searchbar/index.tsx
@@ -1,33 +1,38 @@
 "use client";
 
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { ArrowRightIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
-import { useState, type KeyboardEvent } from "react";
+import { type MouseEvent, useState } from "react";
 
 const Searchbar = () => {
   const [query, setQuery] = useState("");
   const router = useRouter();
 
-  const handleSendQuery = (event: KeyboardEvent<HTMLDivElement>) => {
-    if (query && event.code === "Enter") {
+  const handleSendQuery = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (query) {
       router.push(`/search/${query}`);
     }
   };
 
   return (
-    <section>
+    <form>
       <div className="flex items-center gap-4 rounded bg-card-dark px-5 py-3">
         <MagnifyingGlassIcon className="h-6 w-6" />
         <input
           type="text"
           placeholder="Search Movies and Series"
           value={query}
-          onKeyDown={handleSendQuery}
           onChange={(event) => setQuery(event.target.value)}
           className="h-full w-full bg-transparent focus:outline-none"
         />
+        {query ? (
+          <button onClick={handleSendQuery}>
+            <ArrowRightIcon className="h-6 w-6" />
+          </button>
+        ) : null}
       </div>
-    </section>
+    </form>
   );
 };
 

05cf860618c3d2da1a8c80abdb26db2c0aa5da6b
ntabucejo <ntabucejo@gmail.com>
2022-11-27 19:33:49 +0800

    fix(card): fix hover

--- components/sections/browse/card.tsx
+++ components/sections/browse/card.tsx
@@ -18,12 +18,12 @@ const Card = async ({ media }: Props) => {
       {/* Show on tablet */}
       <li className="hidden space-y-2 tablet:block">
         <Link href={`/${media.type}/${media.id}`}>
-          <div className="relative aspect-video overflow-hidden rounded">
+          <div className="group relative aspect-video overflow-hidden rounded">
             <Image
               src={`https://image.tmdb.org/t/p/w500${media.image.backdrop!}`}
               alt={media.title!}
               fill
-              className="transition-smooth object-cover hover:brightness-[130%]"
+              className="transition-smooth object-cover group-hover:brightness-[130%]"
             />
             <div className="pointer-events-none absolute inset-0 z-10 bg-gradient-to-t from-background-dark to-transparent" />
           </div>
@@ -47,13 +47,13 @@ const Card = async ({ media }: Props) => {
       <li className="tablet:hidden">
         <Link
           href={`/${media.type}/${media.id}`}
-          className="flex items-center gap-4">
+          className="group flex items-center gap-4">
           <div className="relative aspect-video w-24 flex-none overflow-hidden rounded">
             <Image
               src={`https://image.tmdb.org/t/p/w500${media.image.backdrop!}`}
               alt={media.title!}
               fill
-              className="transition-smooth object-cover hover:brightness-[130%]"
+              className="transition-smooth object-cover group-hover:brightness-[130%]"
             />
           </div>
           <p className="text-xs font-semibold">{media.title}</p>

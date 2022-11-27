3827117b3e8bc044c1c0ba2d58f92463dd0fc054
ntabucejo <ntabucejo@gmail.com>
2022-11-27 21:27:08 +0800

    feature(explore): add explore on search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,7 @@
 import Browse from "../../components/sections/browse";
 import Card from "../../components/sections/browse/card";
+import Explore from "../../components/sections/explore";
+import Collection from "../../components/sections/explore/collection";
 import api from "../../library/api";
 import type { Media } from "../../types";
 
@@ -17,13 +19,24 @@ const Page = async () => {
 
   const popularMedias: Media[] = [...popularMovies, ...popularSeries];
 
+  const popularSearch = await api.get.medias.group({
+    name: "top-rated",
+    type: "movies",
+    page: Math.floor(Math.random() * 20) + 5,
+  });
+
   return (
-    <Browse isOnQuery={false}>
-      {popularMedias.map((media: any) => (
-        // @ts-ignore
-        <Card key={media.id} media={media} isOnQuery={false} />
-      ))}
-    </Browse>
+    <div className="content">
+      <Explore>
+        <Collection title="Popular Search" medias={popularSearch} />
+      </Explore>
+      <Browse isOnQuery={false}>
+        {popularMedias.map((media: any) => (
+          // @ts-ignore
+          <Card key={media.id} media={media} isOnQuery={false} />
+        ))}
+      </Browse>
+    </div>
   );
 };
 

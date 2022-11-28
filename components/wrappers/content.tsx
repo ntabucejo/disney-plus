14f9554efbf0293c7543710a3be6b74bd3555915
ntabucejo <ntabucejo@gmail.com>
2022-11-28 15:48:06 +0800

    chore(content): add pt-6

--- components/wrappers/content.tsx
+++ components/wrappers/content.tsx
@@ -14,7 +14,7 @@ const Content = ({ children, variant }: Props) => {
         "-mt-[100px] tablet:-mt-[500px] laptop:-mt-[650px] desktop:-mt-[800px] content";
       break;
     case "secondary":
-      style = "bg-background-dark";
+      style = "bg-background-dark pt-6";
       break;
     case "tertiary":
       style = "pt-4 tablet:pt-8 space-y-4 tablet:space-y-8";

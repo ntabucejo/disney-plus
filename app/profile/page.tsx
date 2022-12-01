07773bdb15760d8d354a2dbdb3c74a5d737a0676
ntabucejo <ntabucejo@gmail.com>
2022-12-01 17:40:57 +0800

    chore(media): add recommendation for user profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,32 @@
+import Cover from "../../components/sections/cover";
+import Explore from "../../components/sections/explore";
+import Collection from "../../components/sections/explore/collection";
 import User from "../../components/sections/user";
 import Content from "../../components/wrappers/content";
+import api from "../../library/api";
 
 const Page = async () => {
+  const popularMovies = await api.get.medias.group({
+    name: "popular",
+    type: "movies",
+    page: 1,
+  });
+  const popularSeries = await api.get.medias.group({
+    name: "popular",
+    type: "series",
+    page: 1,
+  });
+
   return (
     <>
-      <Content variant="tertiary">
+      <Cover company="starwars" isNoImage={false} />
+      <Content variant="secondary">
         {/* @ts-ignore */}
         <User />
+        <Explore>
+          <Collection title="Movies for you" medias={popularMovies} />
+          <Collection title="Series for you" medias={popularSeries} />
+        </Explore>
       </Content>
     </>
   );

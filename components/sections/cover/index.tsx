0d7ffce926691c2f4c3316dcd1e4b7badb9558fc
ntabucejo <ntabucejo@gmail.com>
2022-12-01 19:04:00 +0800

    chore(image): revert logic

--- components/sections/cover/index.tsx
+++ components/sections/cover/index.tsx
@@ -2,10 +2,9 @@ import Image from "next/image";
 
 type Props = {
   company: string;
-  isImageHidden?: boolean;
 };
 
-const Cover = ({ company, isImageHidden }: Props) => {
+const Cover = ({ company }: Props) => {
   return (
     <section className="relative -z-10 grid aspect-[16/4.6] w-full items-center">
       <video className="absolute w-full brightness-110" autoPlay loop muted>
@@ -14,18 +13,15 @@ const Cover = ({ company, isImageHidden }: Props) => {
           type="video/mp4"
         />
       </video>
-      {!isImageHidden ? (
-        <div className="relative z-20 mx-auto aspect-video w-[35%]">
-          <Image
-            src={`/assets/images/${company}-logo.png`}
-            alt="Originals"
-            fill
-            sizes="35%"
-            className="object-contain"
-          />
-        </div>
-      ) : null}
-
+      <div className="relative z-20 mx-auto aspect-video w-[35%]">
+        <Image
+          src={`/assets/images/${company}-logo.png`}
+          alt="Originals"
+          fill
+          sizes="35%"
+          className="object-contain"
+        />
+      </div>
       <div className="absolute inset-0 z-10 hidden bg-gradient-to-r from-background-dark to-transparent tablet:block" />
       <div className="absolute inset-0 z-10 bg-gradient-to-t from-background-dark to-transparent" />
     </section>

f274605e673b002b856a2a0e3aea3d34f0ff3b93
ntabucejo <ntabucejo@gmail.com>
2022-12-01 17:40:20 +0800

    chore(cover): add optional image

--- components/sections/cover/index.tsx
+++ components/sections/cover/index.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 
 type Props = {
   company: string;
+  isNoImage?: boolean;
 };
 
-const Cover = ({ company }: Props) => {
+const Cover = ({ company, isNoImage }: Props) => {
   return (
     <section className="relative -z-10 grid aspect-[16/4.6] w-full items-center">
       <video className="absolute w-full brightness-110" autoPlay loop muted>
@@ -13,15 +14,18 @@ const Cover = ({ company }: Props) => {
           type="video/mp4"
         />
       </video>
-      <div className="relative z-20 mx-auto aspect-video w-[35%]">
-        <Image
-          src={`/assets/images/${company}-logo.png`}
-          alt="Originals"
-          fill
-          sizes="35%"
-          className="object-contain"
-        />
-      </div>
+      {isNoImage ? (
+        <div className="relative z-20 mx-auto aspect-video w-[35%]">
+          <Image
+            src={`/assets/images/${company}-logo.png`}
+            alt="Originals"
+            fill
+            sizes="35%"
+            className="object-contain"
+          />
+        </div>
+      ) : null}
+
       <div className="absolute inset-0 z-10 hidden bg-gradient-to-r from-background-dark to-transparent tablet:block" />
       <div className="absolute inset-0 z-10 bg-gradient-to-t from-background-dark to-transparent" />
     </section>

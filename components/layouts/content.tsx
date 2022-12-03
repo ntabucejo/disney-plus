2916f19045557c0c6ad95c587be30a650175bb1f
ntabucejo <ntabucejo@gmail.com>
2022-12-03 18:58:42 +0800

    chore(style): remove style on css

--- components/layouts/content.tsx
+++ components/layouts/content.tsx
@@ -10,22 +10,25 @@ const Content = ({ children, variant, isSpacerOnly = false }: Props) => {
   let style = "";
 
   switch (variant) {
+    // With media logo
     case "primary":
       style =
         "-mt-[100px] tablet:-mt-[400px] laptop:-mt-[650px] desktop:-mt-[800px]";
       break;
+    // Stack section
     case "secondary":
-      style = "bg-background-dark pt-6 tablet:pt-12 ground";
+      style = "bg-background-dark pt-6 tablet:pt-12 pb-28 tablet:pb-12";
       break;
+    // Example search with custom padding top, Also if there is no cover/logo or content on top
     case "tertiary":
-      style = "pt-4 tablet:pt-8 ground";
+      style = "pt-4 tablet:pt-8 pb-28 tablet:pb-12";
       break;
   }
 
   // Only apply when primary is passed
   if (isSpacerOnly) {
     return (
-      <div className="spacer ground relative bg-background-dark">
+      <div className="relative space-y-6 bg-background-dark pb-28 tablet:space-y-12 tablet:pb-12">
         {children}
       </div>
     );
@@ -33,7 +36,7 @@ const Content = ({ children, variant, isSpacerOnly = false }: Props) => {
 
   return (
     <div
-      className={`${style} spacer transition-smooth px-2 overflow-x-hidden tablet:px-0`}>
+      className={`${style} transition-smooth space-y-6 px-2 overflow-x-hidden tablet:space-y-12 tablet:px-0`}>
       {children}
     </div>
   );
